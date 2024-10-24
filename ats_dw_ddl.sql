60430d6cf7a4dc69770785575af05d2bc1d8d0ca
JRawlings66 <122933457+JRawlings66@users.noreply.github.com>
2024-10-23 17:45:51 -0700

    Update ats_dw_ddl.sql
    added rest of tables

--- ats_dw_ddl.sql
+++ ats_dw_ddl.sql
@@ -33,3 +33,110 @@ CREATE TABLE `Fact_Bond_Prices` (
   FOREIGN KEY (`time_id`) REFERENCES `Dim_Time`(`time_id `),
   FOREIGN KEY (`bond_id`) REFERENCES `Dim_bonds`(`bond_ID`)
 );
+
+CREATE TABLE `Dim_Indexes` (
+  `index_ID` bigint,
+  `historical_ID` bigint,
+  `index_name` varchar(300),
+  `symbol` varchar(10),
+  `exchange` varchar(300),
+  PRIMARY KEY (`index_ID`)
+);
+
+CREATE TABLE `Dim_Commodites` (
+  `commodity_ID` bigint,
+  `commodity_name` varchar(30),
+  `symbol` varchar(10),
+  `exchange` varchar(20),
+  PRIMARY KEY (`commodity_ID`)
+);
+
+CREATE TABLE `Fact_Commodity_Prices` (
+  `fact_id` int,
+  `time_id` int,
+  `commodity_id` bigint,
+  `open` float,
+  `high` float,
+  `low` float,
+  `close` float,
+  `adjClose` float,
+  `volume` bigint,
+  `unadjusted_volume` bigint,
+  `change` float,
+  `change_percentage` float,
+  `vwap` float,
+  `change_over_time` float,
+  PRIMARY KEY (`fact_id`),
+  FOREIGN KEY (`commodity_id`) REFERENCES `Dim_Commodites`(`commodity_ID`)
+);
+
+CREATE TABLE `Fact_Index_Prices` (
+  `fact_id ` int,
+  `time_id` int,
+  `index_id` bigint,
+  `open` float,
+  `high` float,
+  `low` float,
+  `close` float,
+  `volume` bigint,
+  `change_percent` float,
+  `adjClose` float,
+  `unadjustedVolume` bigint,
+  `change` float,
+  `vwap` float,
+  `changeOverTime` float,
+  PRIMARY KEY (`fact_id `),
+  FOREIGN KEY (`index_id`) REFERENCES `Dim_Indexes`(`index_ID`)
+);
+
+CREATE TABLE `Dim_company_statements` (
+  `company_id` bigint,
+  `company_name` varchar(300),
+  `symbol` varchar(10),
+  `beta` float,
+  `volAvg` float,
+  `mktCap` bigint,
+  `lastDiv` float,
+  `changes` float,
+  `currency` varchar(10),
+  `cik` varchar(16),
+  `isin` varchar(16),
+  `cusip` varchar(16),
+  `exchangeFullName` varchar(100),
+  `exchange` varchar(10),
+  `industry` varchar(300),
+  `ceo` varchar(100),
+  `sector` varchar(300),
+  `country` varchar(300),
+  `fullTimeEmployees` bigint,
+  `phone` integer,
+  `address` varchar(100),
+  `city` varchar(100),
+  `state` varchar(30),
+  `zip` varchar(10),
+  `dcfDiff` float,
+  `dcf` float,
+  `ipoDate` date,
+  `isEtf` bool,
+  `isActivelyTrading` bool,
+  `isFund` bool,
+  PRIMARY KEY (`company_id`)
+);
+
+CREATE TABLE `Fact_Stock_Prices` (
+  `fact_id` int,
+  `time_id` int,
+  `company_id` bigint,
+  `open` float,
+  `high` float,
+  `low` float,
+  `close` float,
+  `adj_close` float,
+  `volume` bigint,
+  `change_percent` float,
+  `vwap` float,
+  PRIMARY KEY (`fact_id`),
+  FOREIGN KEY (`company_id`) REFERENCES `Dim_company_statements`(`company_id`)
+);
+
+
