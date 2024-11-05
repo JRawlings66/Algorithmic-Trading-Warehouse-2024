58346c5bd260854dc50e5882ce9585b69863167c
JRawlings66 <jacobrawlings66@gmail.com>
2024-11-04 18:43:08 -0800

    - Amended load_bonds function. Added try block, for loop and csv reader - Added some TODO's - added some more globals - minor formatting

--- data_insertion.py
+++ data_insertion.py
@@ -1,49 +1,62 @@
 import traceback
-import sqlalchemy
-
+import sqlalchemy as db
+import csv
 
 # GLOBALS
-OUTPUT_FILE_PATH = 'ui/output/Bonds-data.csv'
+BONDS_PATH = 'raw_data/Bonds_data.csv'
+COMMODITIES_PATH = 'raw_data/Commodities_data.csv'
+STOCKS_PATH = 'raw_data/Stocks_data.csv'
+INDEXES_PATH = 'raw_data/Indexes_data.csv'
+# Replace username, password, host, dbname with credentials
+DATABASE_URI = 'mysql+pymysql://username:password@host/dbname'
+
+# Initialize connection to db
+engine = db.create_engine(DATABASE_URI, echo=True)
 
 
-def load_bonds(connection, bond_data_raw):
-    '''
+def load_bonds(connection, bonds_data_file=BONDS_PATH):
+    """
     ETL function for treasury bond data.
-    param:
-    return:
-    '''
-    with open(OUTPUT_FILE_PATH, "r") as bonds_data:
-        insert_query = sqlalchemy.text("INSERT INTO `Fact_Bond_Prices` VALUES (:bond_id, :one_month, :two_month, "
-                                       ":three_month, :six_month, :one_year, :two_year, :three_year, :five_year, "
-                                       ":ten_year, :twenty_year, :thirty_year)")
-    return None
+    loads csv data and inserts into Fact_Bond_Prices
+    TODO: Insertion process for 'Dim_Bonds'
+    """
+    with open(bonds_data_file, "r") as bonds_data:
+        csv_loader = csv.DictReader(bonds_data)
+        for row in csv_loader:
+            try:
+                insert_query = db.text("""
+                    INSERT INTO Fact_Bond_Prices (bond_id, one_month, two_month, three_month, six_month, one_year, 
+                    two_year, three_year, five_year, ten_year, twenty_year, thirty_year)
+                    VALUES (:bond_id, :one_month, :two_month, :three_month, :six_month, :one_year, :two_year, 
+                    :three_year, :five_year, :ten_year, :twenty_year, :thirty_year)
+                """)
+            except Exception as e:
+                print("Error when inserting bonds:", e)
+                traceback.print_exc()
+
 
 def load_commodities(connection, commodity_data_raw):
-    '''
-    ETL function for commodity data.
-    param:
-    return:
-    '''
-    with open(OUTPUT_FILE_PATH, "r") as commodity_data:
-        insert_query = sqlalchemy.text("INSERT INTO `Fact_Commodity_Prices VALUES ()`")
+    """
+    TODO: ETL function for commodity data.
+    """
+    with open(COMMODITIES_PATH, "r") as commodity_data:
+        insert_query = db.text("""INSERT INTO Fact_Commodity_Prices VALUES ()`""")
     return None
+
 
 def load_stocks(connection, stock_data_raw):
-    '''
-    ETL function for stock data.
-    param:
-    return:
-    '''
-    with open(OUTPUT_FILE_PATH, "r") as stock_data:
-        insert_query = sqlalchemy.text("INSERT INTO `Fact_Stock_Prices VALUES ()")
+    """
+    TODO: ETL function for stock data.
+    """
+    with open(STOCKS_PATH, "r") as stock_data:
+        insert_query = db.text("""INSERT INTO Fact_Stock_Prices VALUES ()""")
     return None
 
+
 def load_indexes(connection, index_data_raw):
-    '''
-    ETL function for index data.
-    param:
-    return:
-    '''
-    with open(OUTPUT_FILE_PATH, "r") as index_data:
-        insert_query = sqlalchemy.text("INSERT INTO `Fact_Index_Prices VALUES ()")
+    """
+    TODO: ETL function for index data.
+    """
+    with open(INDEXES_PATH, "r") as index_data:
+        insert_query = db.text("""INSERT INTO Fact_Index_Prices VALUES ()""")
     return None
